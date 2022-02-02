c7b2650297b9c273e432f38d7066a0c282ffea25
UrbinaBarrios <urbinabarios@gmail.com>
2022-02-02 16:40:59 -0400

    Agregado room routes, room controller, server api para room

--- src/controllers/room/room.controller.js
+++ src/controllers/room/room.controller.js
@@ -1,1 +1,103 @@
-import Room from "../models/room";
+const Hotel = require("../../models/hotel");
+const Room = require("../../models/room");
+const { successResponse, errorResponse } = require("../../helpers/index");
+
+// CREATE
+const createRoom = async (req, res) => {
+  try {
+    const { name, type, size, num_bed, limit, quantity, id_hotel } = req.body;
+    console.log(req.body);
+    const room = await Room.create({
+      name: name,
+      type: type,
+      size: size,
+      num_bed: num_bed,
+      limit: limit,
+      quantity: quantity,
+      active: true,
+      id_hotel: id_hotel,
+    });
+    return successResponse(req, res, room);
+  } catch (error) {
+    return errorResponse(req, res, error.message);
+  }
+};
+
+// GET ALL (READ)
+const getAllRooms = async (req, res) => {
+  try {
+    const rooms = await Room.findAll({
+      attributes: [
+        "id",
+        "name",
+        "type",
+        "size",
+        "num_bed",
+        "limit",
+        "quantity",
+        "id_hotel",
+      ],
+      where: { active: true },
+    });
+    return successResponse(req, res, rooms);
+  } catch (error) {
+    return errorResponse(req, res, error.message);
+  }
+};
+
+// GET ONE/MANY
+const getManyRooms = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const rooms = await Room.findAll({
+      where: { name: name, active: true },
+    });
+    return successResponse(req, res, rooms);
+  } catch (error) {
+    return errorResponse(req, res, error.message);
+  }
+};
+
+// GET BY Id
+const getRoomById = async (req, res) => {
+  let id = req.params.id;
+  try {
+    const room = await Room.findAll({
+      where: { id: id, active: true },
+    });
+    return successResponse(req, res, room);
+  } catch (error) {
+    return errorResponse(req, res, error.message);
+  }
+};
+
+// UPDATE
+const updateRoom = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const room = await Room.update(req.body, { where: { id: id } });
+    return successResponse(req, res, room);
+  } catch (error) {
+    return errorResponse(req, res, error.message);
+  }
+};
+
+// DELETE (LOGICO) *TO-DO*
+const deleteLogicallyRoom = async (req, res) => {
+  try {
+    let id = req.params.id;
+    const room = await Room.update({ active: false }, { where: { id: id } });
+    return successResponse(req, res, room);
+  } catch (error) {
+    return errorResponse(req, res, error.message);
+  }
+};
+
+module.exports = {
+  createRoom,
+  updateRoom,
+  getRoomById,
+  getManyRooms,
+  getAllRooms,
+  deleteLogicallyRoom,
+};
